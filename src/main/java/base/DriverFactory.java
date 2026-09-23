bd6031777e7a56737f6af649bcd7867304bc4445
yogeshsekar24-cyber <yogesh.sekar24@gmail.com>
2026-09-23 22:39:44 +0530

    Update DriverFactory.java

--- src/main/java/base/DriverFactory.java
+++ src/main/java/base/DriverFactory.java
@@ -1,5 +1,5 @@
 package base;
-
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
