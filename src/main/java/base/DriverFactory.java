61be2e8649c3cb92da712f75a79b1ec433b44525
yogeshsekar24-cyber <yogesh.sekar24@gmail.com>
2026-09-23 21:55:01 +0530

    Update DriverFactory.java

--- src/main/java/base/DriverFactory.java
+++ src/main/java/base/DriverFactory.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.Dimension;
 
 public class DriverFactory {
 	private static ThreadLocal<WebDriver> driver =
