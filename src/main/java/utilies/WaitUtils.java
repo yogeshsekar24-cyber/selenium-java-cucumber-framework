836d1bb825cef4caf62e7a169eaa21563c07814c
Yogesh Sekar <yogesh.sekar24@gmail.com>
2026-09-23 20:55:13 +0530

    Add screenshot evidence and set wait timeout

--- src/main/java/utilies/WaitUtils.java
+++ src/main/java/utilies/WaitUtils.java
@@ -12,7 +12,7 @@ public class WaitUtils {
 	private WebDriverWait wait;
 	
 	public WaitUtils(WebDriver driver) {
-		wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 	}
 	
 	public WebElement waitForVisibility(By locator) {
